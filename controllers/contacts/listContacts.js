0d7302b93c13dc90998f72f66d2a70931f7b9879
Nadiia Tsytsylina <miniova95@gmail.com>
2023-08-23 12:20:28 +0300

    update listContacts

--- controllers/contacts/listContacts.js
+++ controllers/contacts/listContacts.js
@@ -2,11 +2,10 @@ const { Contact } = require('../../models/contact');
 
 const listContacts = async (req, res) => {
   const { _id: owner } = req.user;
-  const { page = 1, limit = 20, favorite = false } = req.query;
-  // не зрозуміла, як зробити так, щоб при запиті на /contacts показувались всі контакти (і true, і false)
+  const { page = 1, limit = 20, favorite } = req.query;
   const skip = (page - 1) * limit;
   const result = await Contact.find(
-    { owner, favorite },
+    favorite ? { owner, favorite } : { owner },
     '-createdAt -updatedAt',
     {
       skip,
