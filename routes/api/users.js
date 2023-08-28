463e15f520a28dd87c29183cf8402136e2478177
Nadiia Tsytsylina <miniova95@gmail.com>
2023-08-28 18:16:50 +0300

    add hw#5

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { validateBody, authenticate } = require('../../middlewares');
+const { validateBody, authenticate, upload } = require('../../middlewares');
 const { schemas } = require('../../models/user');
 const ctrl = require('../../controllers/users');
 
@@ -18,6 +18,13 @@ router.patch(
   authenticate,
   validateBody(schemas.updateSubscriptionSchema),
   ctrl.updateSubscription
+);
+
+router.patch(
+  '/avatars',
+  authenticate,
+  upload.single('avatar'),
+  ctrl.updateAvatar
 );
 
 module.exports = router;
